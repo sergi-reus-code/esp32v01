9e969e0b8009e6a56ed1d67942c80cde7e849987
sergi-reus-code <sergi@reusconstruccions.com>
2022-08-26 09:11:54 +0200

    gfhdfg

--- src/main.cpp
+++ src/main.cpp
@@ -18,14 +18,13 @@ static const char *TAG = "AW";
 
 static TaskHandle_t ledTask_handle;
 
-
 void wait_for_button_push()
 {
   while (gpio_get_level(GPIO_BUTTON) == 0)
   {
-    ESP_LOGI(TAG, "Button pressed");
-    vTaskDelay(pdMS_TO_TICKS(100));
+    vTaskDelay(pdMS_TO_TICKS(1000));
   }
+  ESP_LOGI(TAG, "Button pressed");
 }
 
 void record(I2SSampler *input, const char *fname)
@@ -93,33 +92,30 @@ void main_task(void *param)
 #ifdef USE_SPIFFS
   ESP_LOGI(TAG, "Mounting SPIFFS on /sdcard");
 
-  //SPIFFS.begin(false,"/sdcard");
+  // SPIFFS.begin(false,"/sdcard");
 
   SPIFFS.totalBytes();
   SPIFFS.begin(true, "/sdcard");
-  bool resp = SPIFFS.exists("/Hello.mp3");
+  bool resp = SPIFFS.exists("/test.wav");
   Serial.println(resp);
 
-  
 #else
   ESP_LOGI(TAG, "Mounting SDCard on /sdcard");
   new SDCard("/sdcard", PIN_NUM_MISO, PIN_NUM_MOSI, PIN_NUM_CLK, PIN_NUM_CS);
 #endif
 
-  //ESP_LOGI(TAG, "Creating microphone");
+  // ESP_LOGI(TAG, "Creating microphone");
 #ifdef USE_I2S_MIC_INPUT
-  //I2SSampler *input = new I2SMEMSSampler(I2S_NUM_0, i2s_mic_pins, i2s_mic_Config);
+  // I2SSampler *input = new I2SMEMSSampler(I2S_NUM_0, i2s_mic_pins, i2s_mic_Config);
 #else
   I2SSampler *input = new ADCSampler(ADC_UNIT_1, ADC1_CHANNEL_7, i2s_adc_config);
 #endif
 
 #ifdef USE_I2S_SPEAKER_OUTPUT
-  //Output *output = new I2SOutput(I2S_NUM_0, i2s_speaker_pins);
+  Output *output = new I2SOutput(I2S_NUM_0, i2s_speaker_pins);
 #else
   Output *output = new DACOutput(I2S_NUM_0);
 #endif
-
-
 
   while (true)
   {
@@ -127,13 +123,14 @@ void main_task(void *param)
 
     // wait for the user to push and hold the button
     wait_for_button_push();
+
     ESP_LOGI(TAG, "Recording");
-    //record(input, "/sdcard/test.wav");
-    // wait for the user to push the button again
+    // record(input, "/sdcard/test.wav");
+    //  wait for the user to push the button again
     wait_for_button_push();
     ESP_LOGI(TAG, "Playing");
-    //play(output, "/sdcard/test.wav");
-    
+    play(output, "/sdcard/test.wav");
+
     vTaskDelay(pdMS_TO_TICKS(1000));
   }
 }
@@ -143,9 +140,9 @@ void setup()
   Serial.begin(115200);
   vTaskDelay(pdMS_TO_TICKS(1000));
 
-    gpio_set_direction(GPIO_BUTTON, GPIO_MODE_INPUT);
+  gpio_set_direction(GPIO_BUTTON, GPIO_MODE_INPUT);
   gpio_set_pull_mode(GPIO_BUTTON, GPIO_PULLDOWN_ONLY);
-  
+
   xTaskCreate(main_task, "Main", 4096, NULL, 0, NULL);
 }
 
