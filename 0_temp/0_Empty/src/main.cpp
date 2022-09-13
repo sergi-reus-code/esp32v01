039fd9cc5c21bda9f7659305f7ef008517655496
sergi-reus-code <sergi@reusconstruccions.com>
2022-09-13 17:20:49 +0200

    dfdb

--- 0_temp/0_Empty/src/main.cpp
+++ 0_temp/0_Empty/src/main.cpp
@@ -7,18 +7,13 @@
 
 #include "config.h"
 
-static const char *TAG = "DEEP SLEEP";
+#include <Arduino.h>
 
-
-
-
-
-void setup()
-{
-  Serial.begin(115200);
-ESP_LOGI(TAG, "Init");
+void setup() {
+  log_d("Total heap: %d", ESP.getHeapSize());
+  log_d("Free heap: %d", ESP.getFreeHeap());
+  log_d("Total PSRAM: %d", ESP.getPsramSize());
+  log_d("Free PSRAM: %d", ESP.getFreePsram());
 }
 
-void loop()
-{
-}
+void loop() {}
