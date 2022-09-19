8a239ba3666a5c5700d67be8bcb468fca8f587fe
Sergi Reus <sergireus28@gmail.com>
2022-09-19 09:16:16 +0200

    fghd

--- 0_temp/6_Notifications/src/main.cpp
+++ 0_temp/6_Notifications/src/main.cpp
@@ -36,13 +36,51 @@ void receiver(void *params)
   }
 }
 
+void ledsTask(void *params)
+{
+  uint state;
+  while (true)
+  {
+
+    xTaskNotifyWait(0xffffffff, 0, &state, portMAX_DELAY);
+    //Serial.println(eTaskGetState(receiverHandler));
+    //printf("received state %d times\n", state);
+
+  }
+}
+
+
+
+
+
+
+
+
 void setup(void)
 {
   Serial.begin(115200);
+  //Crear todas las tareas -> Dentro de las tareas crear los objetos -> Ponerlos en wait
+
+  //Check todas las tareas estan en bloqued 
+
   xTaskCreate(&receiver, "receiver", 2048, NULL, 2, &receiverHandler);
   xTaskCreate(&sender, "sender", 2048, NULL, 2, NULL);
+delay(3000);
+  //while conversa not finiquitada ir haciendo while
+  Serial.println("----------------------------");
+Serial.println(eTaskGetState(receiverHandler));
+
+  //si consversa finiquitada porner en deep sleeep
+
+
+
+
+
+
 }
 
 void loop(){
+
+    vTaskDelete(NULL);
 
 }
