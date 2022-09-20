cb8ea14952d0408b97eca688c6aee55e0de21af5
Sergi Reus <sergireus28@gmail.com>
2022-09-20 11:17:19 +0200

    gyuyui

--- 0_temp/6_Notifications/src/main.cpp
+++ 0_temp/6_Notifications/src/main.cpp
@@ -106,7 +106,7 @@ void recordTask(void *params)
     
     delay(100);
     log_i("111111111111111111", TAG);
-    xTaskNotifyWait(0xffffffff, 0, &state, portMAX_DELAY);
+    //xTaskNotifyWait(0xffffffff, 0, &state, portMAX_DELAY);
     log_i("111111111111111111", TAG);
     delay(100);
     Serial.println("pepepepeep");
@@ -125,21 +125,21 @@ void mainTask(void *params)
 
   //xTASK_STATUS statustask = NULL;
   Serial.println("En main Task");
-  log_i("dssadfasdf", TAG);
-  delay(100);
+  //log_i("dssadfasdf", TAG);
+  delay(2000);
   while (true) {
-    delay(2000);
+    delay(1000);
     Serial.println("EEEEEstado de la tarea " + (String)eTaskGetState(&recordHandler));
-    xTaskNotify(&recordHandler, (1 << 1), eSetBits);
+    //xTaskNotify(&recordHandler, (1 << 1), eSetBits);
+    //xTaskNotify(&buttonHandler, (1 << 1), eSetBits);
+    //xTaskNotify(&ledsHandler, (1 << 1), eSetBits);
+    //xTaskNotify(&sdHandler, (1 << 1), eSetBits);
+    //xTaskNotify(&speakerHandler, (1 << 1), eSetBits);
+
     Serial.println("AAAAAEstado de la tarea " + (String)eTaskGetState(&recordHandler));
     
   }
 }
-
-
-
-
-
 
 
 void setup(void)
@@ -151,25 +151,22 @@ void setup(void)
 static TaskHandle_t mainHandler = NULL;
 static TaskHandle_t recordHandler = NULL;
 
-//static TaskHandle_t buttonHandler = NULL;
-//static TaskHandle_t ledsHandler = NULL;
-//static TaskHandle_t sdHandler = NULL;
-//static TaskHandle_t speakerHandler = NULL;
+static TaskHandle_t buttonHandler = NULL;
+static TaskHandle_t ledsHandler = NULL;
+static TaskHandle_t sdHandler = NULL;
+static TaskHandle_t speakerHandler = NULL;
 
 
 
-xTaskCreate(&receiver, "receiver", 2048, NULL, 2, &receiverHandler);
-xTaskCreate(&sender, "sender", 2048, NULL, 2, NULL);
+//xTaskCreate(&receiver, "receiver", 2048, NULL, 2, &receiverHandler);
+//xTaskCreate(&sender, "sender", 2048, NULL, 2, NULL);
 
-xTaskCreate(&mainTask, "mainTask", 2048, NULL, 2, &mainHandler);
-xTaskCreate(&recordTask, "recordTask", 2048, NULL, 2, &recordHandler);
-
-
-//xTaskCreate(&buttonTask, "sender", 2048, NULL, 2, &buttonHandler);
-//xTaskCreate(&ledsTask, "sender", 2048, NULL, 2, &ledsHandler);
-
-//xTaskCreate(&sdTask, "sender", 2048, NULL, 2, &sdHandler);
-//xTaskCreate(&speakerTask, "sender", 2048, NULL, 2, &speakerHandler);
+xTaskCreate(&mainTask, "mainTask", 1024, NULL, 2, &mainHandler);
+xTaskCreate(&recordTask, "recordTask", 1024, NULL, 2, &recordHandler);
+//xTaskCreate(&buttonTask, "buttonTask", 1024, NULL, 2, &buttonHandler);
+//xTaskCreate(&ledsTask, "ledsTask", 1024, NULL, 2, &ledsHandler);
+//xTaskCreate(&sdTask, "sdTask", 1024, NULL, 2, &sdHandler);
+//xTaskCreate(&speakerTask, "speakerTask", 1024, NULL, 2, &speakerHandler);
 
 //Check todas las tareas estan en bloqued
 
