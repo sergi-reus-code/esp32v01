de1f18e57cafe625901607e0bb19fc01d171b41b
sergi-reus-code <sergi@reusconstruccions.com>
2022-11-03 08:00:58 +0100

    bnvmbnm

--- 0_temp/idf-wav-sdcard_working/src/config.h
+++ 0_temp/idf-wav-sdcard_working/src/config.h
@@ -8,14 +8,23 @@
 // sample rate for the system
 #define SAMPLE_RATE 16000
 
+#define USE_I2S_MIC_INPUT
+
 // I2S Microphone Settings
 // Which channel is the I2S microphone on? I2S_CHANNEL_FMT_ONLY_LEFT or I2S_CHANNEL_FMT_ONLY_RIGHT
 // Generally they will default to LEFT - but you may need to attach the L/R pin to GND
 #define I2S_MIC_CHANNEL I2S_CHANNEL_FMT_ONLY_LEFT
 // #define I2S_MIC_CHANNEL I2S_CHANNEL_FMT_ONLY_RIGHT
-#define I2S_MIC_SERIAL_CLOCK GPIO_NUM_32
-#define I2S_MIC_LEFT_RIGHT_CLOCK GPIO_NUM_25
-#define I2S_MIC_SERIAL_DATA GPIO_NUM_33
+//#define I2S_MIC_SERIAL_CLOCK GPIO_NUM_32
+//#define I2S_MIC_LEFT_RIGHT_CLOCK GPIO_NUM_25
+//#define I2S_MIC_SERIAL_DATA GPIO_NUM_33
+
+
+#define I2S_MIC_SERIAL_CLOCK GPIO_NUM_26
+#define I2S_MIC_LEFT_RIGHT_CLOCK GPIO_NUM_22
+#define I2S_MIC_SERIAL_DATA GPIO_NUM_21
+
+
 
 // speaker settings
 #define I2S_SPEAKER_SERIAL_CLOCK GPIO_NUM_19
